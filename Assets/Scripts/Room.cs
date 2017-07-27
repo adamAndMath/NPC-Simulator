2b7e1619a4265cf6561537447d34e139238fbb89
Adam <adam.and.math@gmail.com>
2017-07-27 18:59:33 +0200

    Added room script

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public Vector2 size;
+
+    void OnDrawGizmos ()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.right * size.x);
+        Gizmos.DrawLine(transform.position + Vector3.up * size.y, transform.position + (Vector3)size);
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.up * size.y);
+        Gizmos.DrawLine(transform.position + Vector3.right * size.x, transform.position + (Vector3)size);
+    }
+}
