33b87001c860f7a1193d36f680d6bd23bf228411
Erik Funder Carstensen <fundererik@gmail.com>
2017-07-28 14:03:14 +0200

    NPC base

---
+++ Assets/Scripts/NPC/NPC.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC : MonoBehaviour {	
+	public enum Mood
+	{
+		Angry, Sad, Happy, Neutral
+	}
+	
+	public string name;
+	public Mood mood;
+	public float[] needs;
+	
+	// Use this for initialization
+	void Start () {
+		name = Namegenerator.GenerateName();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+	
+	private Vector2 Pos { get {return transform.position;} }
+}
