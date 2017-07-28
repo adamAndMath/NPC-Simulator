3321b208d8ff81bf0fc07afd16ea426455262474
Adam <adam.and.math@gmail.com>
2017-07-28 13:04:25 +0200

    Registeres wall of rooms in World

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -1,13 +1,71 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class Room : MonoBehaviour
 {
     public Position size;
 
+    private readonly List<Connection> connections = new List<Connection>();
+
     public Position Min { get { return transform.position; } }
     public Position Max { get { return Min + size; } }
 
-    void OnDrawGizmos ()
+    public void RegisterConnection(Connection connection)
+    {
+        connections.Add(connection);
+    }
+
+    public void RemoveConnection(Connection connection)
+    {
+        connections.Remove(connection);
+    }
+
+    void OnEnable()
+    {
+        Position pos = new Position();
+
+        for (pos.x = Min.x; pos.x <= Max.x; pos.x++)
+        {
+            pos.y = Min.y;
+            if (World.Instance[pos] == null && connections.All(con => con.position != pos))
+                World.Instance[pos] = gameObject;
+            pos.y = Max.y;
+            if (World.Instance[pos] == null && connections.All(con => con.position != pos))
+                World.Instance[pos] = gameObject;
+        }
+
+        for (pos.y = Min.y; pos.y <= Max.y; pos.y++)
+        {
+            pos.x = Min.x;
+            if (World.Instance[pos] == null && connections.All(con => con.position != pos))
+                World.Instance[pos] = gameObject;
+            pos.x = Max.x;
+            if (World.Instance[pos] == null && connections.All(con => con.position != pos))
+                World.Instance[pos] = gameObject;
+        }
+    }
+
+    void OnDisable()
+    {
+        for (int x = Min.x; x <= Max.x; x++)
+        {
+            if (World.Instance[x, Min.y] == gameObject)
+                World.Instance[x, Min.y] = null;
+            if (World.Instance[x, Max.y] == gameObject)
+                World.Instance[x, Max.y] = null;
+        }
+
+        for (int y = Min.y + 1; y < Max.y; y++)
+        {
+            if (World.Instance[Min.x, y] == gameObject)
+                World.Instance[Min.x, y] = null;
+            if (World.Instance[Max.x, y] == gameObject)
+                World.Instance[Max.x, y] = null;
+        }
+    }
+
+    void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawLine(transform.position, transform.position + Vector3.right * size.x);
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class World : MonoBehaviour
 {
     public static World Instance { get; private set; }
 
     private readonly Dictionary<Position, GameObject> Objects = new Dictionary<Position, GameObject>();
 
+    public GameObject this[int x,  int y]
+    {
+        get { return this[new Position(x, y)]; }
+        set { this[new Position(x, y)] = value; }
+    }
+
     public GameObject this[Position p]
     {
-        get { return Objects[p]; }
+        get { return Objects.ContainsKey(p) ? Objects[p] : null; }
         set
         {
+            if (value == null)
+            {
+                Objects.Remove(p);
+                return;
+            }
+
             if (Objects.ContainsKey(p))
                 throw new Exception(p + " is already ocupied");
 
@@ -20,8 +33,19 @@ public class World : MonoBehaviour
         }
     }
 
-    void OnEnabled()
+    void OnEnable()
     {
         Instance = this;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        foreach (Vector2 pos in Objects.Keys)
+        {
+            Gizmos.DrawLine(pos + new Vector2(-0.2F, -0.2F), pos + new Vector2(0.2F, 0.2F));
+            Gizmos.DrawLine(pos + new Vector2(-0.2F, 0.2F), pos + new Vector2(0.2F, -0.2F));
+        }
     }
 }
